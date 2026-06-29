79de377dc60ae200cc2a2219452e0db3ebd1bc35
Luca <cremoneseluca07@gmail.com>
2026-06-29 16:31:34 +0200

    New animation

--- client/src/components/Workspace.tsx
+++ client/src/components/Workspace.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import type { ArchitectureResult, AgentId } from '@shared/types'
 import { ArchitecturePanel } from './ArchitecturePanel'
 import { CodeDiffPanel } from './CodeDiffPanel'
@@ -15,20 +16,28 @@ export function Workspace({ result, streamedCode, activeDiffView, selectedAgentI
 
   return (
     <main className="flex-1 flex gap-3 p-3 h-full min-h-0 min-w-0">
-      <div className={`${archFlex} flex flex-col min-w-0`}>
+      <motion.div
+        layout
+        transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+        className={`${archFlex} flex flex-col min-w-0`}
+      >
         <ArchitecturePanel
           diagramSvg={result?.diagramSvg ?? null}
           diagramMermaid={result?.diagramMermaid ?? null}
         />
-      </div>
-      <div className={`${codeFlex} flex flex-col min-w-0`}>
+      </motion.div>
+      <motion.div
+        layout
+        transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+        className={`${codeFlex} flex flex-col min-w-0`}
+      >
         <CodeDiffPanel
           code={result?.code ?? null}
           diffLines={result?.diffLines ?? null}
           streamedCode={streamedCode}
           defaultTab={activeDiffView ? 'diff' : undefined}
         />
-      </div>
+      </motion.div>
     </main>
   )
 }
